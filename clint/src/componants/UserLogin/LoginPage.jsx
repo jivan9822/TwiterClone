9fd1f48adc76844e23f58e9d25c0770b0c7d6950
jivan9822 <j.toshniwal@yahoo.co.in>
2023-04-08 12:19:29 +0530

    user form validation done

--- clint/src/componants/UserLogin/LoginPage.jsx
+++ clint/src/componants/UserLogin/LoginPage.jsx
@@ -1,19 +1,48 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import classes from './userreglog.module.css';
 
 function LoginPage() {
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onUserInputHandler = (e) => {
+    const { name, value } = e.target;
+    if (name === 'name') {
+      setUserName(value);
+    }
+    if (name === 'password') {
+      setPassword(value);
+    }
+    if (userName.length > 2 && password.length > 2) {
+      setBtnDisabled(false);
+    } else {
+      setBtnDisabled(true);
+    }
+  };
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log(userName, password);
+  };
   return (
-    <form>
+    <form onSubmit={onSubmitHandler}>
       <h1>Login</h1>
       <div>
         <label htmlFor='name'>Username:</label>
-        <input type='text' name='name' />
+        <input type='text' name='name' onChange={onUserInputHandler} />
       </div>
       <div>
         <label htmlFor='password'>Password:</label>
-        <input type='password' name='password' />
+        <input type='password' name='password' onChange={onUserInputHandler} />
       </div>
-      <button type='submit'>Login</button>
+      <button
+        type='submit'
+        disabled={btnDisabled}
+        className={btnDisabled ? classes.btnNotAllowed : classes.btn}
+      >
+        Login
+      </button>
       <Link to='/register'>Register?</Link>
     </form>
   );
--- clint/src/componants/UserLogin/UserRagistration.jsx
+++ clint/src/componants/UserLogin/UserRagistration.jsx
@@ -1,26 +1,112 @@
+import { useRef, useState } from 'react';
 import classes from './userreglog.module.css';
 import { Link } from 'react-router-dom';
 
+let pass, confPass;
+const isValidInputs = ({ password, confirmPassword }) => {
+  if (password !== confirmPassword) {
+    return false;
+  }
+  return true;
+};
+
 const UserRegistration = (props) => {
+  const [userInputs, setUserInputs] = useState({
+    userName: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const confirmPassRef = useRef(null);
+  const [btnDisabled, setBtnDisabled] = useState(true);
+
+  const onUserInputHandler = (e) => {
+    const { name, value } = e.target;
+    if (name === 'password') {
+      pass = value;
+    }
+    if (name === 'confirmPassword') {
+      confPass = value;
+    }
+    setUserInputs((old) => {
+      return {
+        ...old,
+        [name]: value,
+      };
+    });
+    if (pass && confPass && pass === confPass) {
+      setBtnDisabled(false);
+    } else {
+      setBtnDisabled(true);
+    }
+  };
+
+  const onFormSubmitHandler = (e) => {
+    e.preventDefault();
+    if (isValidInputs(userInputs)) {
+      console.log('Valid');
+    } else {
+      alert('Confirm Password is not same with password!');
+      confirmPassRef.current.focus();
+    }
+    setUserInputs({
+      userName: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    });
+  };
   return (
-    <form>
+    <form onSubmit={onFormSubmitHandler}>
       <div className={classes['form-group']}>
         <label htmlFor='name'>UserName: </label>
-        <input type='text' name='name' />
+        <input
+          type='text'
+          name='userName'
+          value={userInputs.userName}
+          onChange={onUserInputHandler}
+          required
+        />
       </div>
       <div className={classes['form-group']}>
         <label htmlFor='email'>Email Id: </label>
-        <input type='text' name='email' />
+        <input
+          type='email'
+          name='email'
+          value={userInputs.email}
+          onChange={onUserInputHandler}
+          required
+        />
       </div>
       <div className={classes['form-group']}>
         <label htmlFor='password'>Password: </label>
-        <input type='text' name='password' />
+        <input
+          type='password'
+          name='password'
+          value={userInputs.password}
+          onChange={onUserInputHandler}
+          required
+        />
       </div>
       <div className={classes['form-group']}>
         <label htmlFor='confirmPassword'>Confirm Password: </label>
-        <input type='text' name='confirmPassword' />
+        <input
+          type='password'
+          name='confirmPassword'
+          value={userInputs.confirmPassword}
+          onChange={onUserInputHandler}
+          ref={confirmPassRef}
+          required
+        />
       </div>
-      <button type='submit'>Register</button>
+      <button
+        type='submit'
+        disabled={btnDisabled}
+        className={btnDisabled ? classes.btnNotAllowed : classes.btn}
+      >
+        Register
+      </button>
       <Link to='/'>Already Register?</Link>
     </form>
   );
