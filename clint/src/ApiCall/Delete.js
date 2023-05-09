af18e874dc214d3aa021781e77a5afa419899bd3
jivan9822 <j.toshniwal@yahoo.co.in>
2023-05-09 20:05:20 +0530

    SOME MINOR BUG FIX 3

--- clint/src/ApiCall/Delete.js
+++ clint/src/ApiCall/Delete.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { postAction } from '../../Store/Slices/post-slice';
-import { userAction } from '../../Store/Slices/User-Slice';
+import { userAction } from '../../Store/Slices/user-Slice';
 
 const PROXY = import.meta.env.VITE_PROXY;
 
